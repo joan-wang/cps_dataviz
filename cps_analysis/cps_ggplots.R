77cd39f65ea210a1589e873541e3cf2b2fd1a888
joan-wang <joanyiqunwang@gmail.com>
2017-10-20 18:19:11 -0500

    progress

--- cps_analysis/cps_ggplots.R
+++ cps_analysis/cps_ggplots.R
@@ -4,8 +4,12 @@ library(reshape2)
 mytheme <- theme(panel.background = element_rect(fill = "grey80"), panel.grid.major.y = element_blank(),
                  panel.grid.minor.y = element_blank())
 
+mobility <- read_csv('../cps_data/Mobility 01-16.csv')
+profile <- read_csv('../cps_data/Profile_1617.csv')
 combined_1617 <- read_csv('../cps_data/Combined_1617.csv')
 elem_1617 <- filter(combined_1617, Primary_Category == 'ES')
+attendance <- read_csv('../cps_data/Attendance_03-17.csv')
+attendance <-select(filter(attendance, Group == "All (Excludes Pre-K)"), -Grade)
 
 # Scatter: second year attendance vs reading attainment score
 attainment <- melt(select(elem_1617, Student_Attendance_Year_2_Pct, School_ID, Attainment_Reading_Pct_ES, Attainment_Math_Pct_ES), id.vars = c("School_ID", "Student_Attendance_Year_2_Pct"),
@@ -25,4 +29,31 @@ ggplot(data = attainment, aes(x = Student_Attendance_Year_2_Pct, y = attainment_
      caption = "Source: Chicago Public Schools - School Progress Reports SY1617") + 
   mytheme
 
-# change in attendance between the two years
+# Line: attendance over time
+
+avg_attendance <- melt(attendance, id.vars = c("School ID", "School Name",	"Network", "Group"),
+                    variable.name = "year", 
+                    value.name = "attendance_rate") %>%
+  rename("School_ID" = "School ID") %>%
+  inner_join(profile, by="School_ID") %>%
+  group_by(year, Zip) %>%
+  summarise(attendance_rate = mean(attendance_rate, na.rm = TRUE))
+
+avg_attendance$Zip <- factor(avg_attendance$Zip)
+
+ggplot(data = avg_attendance, aes(x = year, y = attendance_rate, color = factor(Zip))) + 
+  geom_line(aes(group = Zip))
+
+
+# Line: mobility over time
+profile$School_ID <- as.character(profile$School_ID)
+avg_mobility <- melt(mobility, id.vars = c("School ID", "School Name"),
+                       variable.name = "year", 
+                       value.name = "mobility_rate") %>%
+  rename("School_ID" = "School ID") %>%
+  inner_join(profile, by="School_ID") %>%
+  group_by(year, Zip) %>%
+  summarise(mobility_rate = mean(mobility_rate, na.rm = TRUE))
+
+ggplot(data = avg_mobility, aes(x = year, y = mobility_rate, color = factor(Zip))) + 
+  geom_line(aes(group = Zip))
